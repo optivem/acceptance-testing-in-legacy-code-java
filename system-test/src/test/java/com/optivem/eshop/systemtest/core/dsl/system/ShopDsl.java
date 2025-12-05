853b7741d5e0eda57106172396323d16ab3f75c5
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-05 16:20:34 +0100

    Add command classes for order management: CancelOrder, ConfirmOrderPlaced, GoToShop, PlaceOrder, and ViewOrder

--- system-test/src/test/java/com/optivem/eshop/systemtest/core/dsl/system/ShopDsl.java
+++ system-test/src/test/java/com/optivem/eshop/systemtest/core/dsl/system/ShopDsl.java
@@ -2,10 +2,10 @@ package com.optivem.eshop.systemtest.core.dsl.system;
 
 import com.optivem.eshop.systemtest.core.drivers.system.ShopDriver;
 import com.optivem.eshop.systemtest.core.dsl.commons.DslContext;
-import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.CancelOrderCommand;
-import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.GoToShopCommand;
-import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.PlaceOrderCommand;
-import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.ViewOrderCommand;
+import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.CancelOrder;
+import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.GoToShop;
+import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.PlaceOrder;
+import com.optivem.eshop.systemtest.core.dsl.system.commands.execute.ViewOrder;
 
 public class ShopDsl {
     private final ShopDriver driver;
@@ -16,20 +16,20 @@ public class ShopDsl {
         this.context = context;
     }
 
-    public GoToShopCommand goToShop() {
-        return new GoToShopCommand(driver, context);
+    public GoToShop goToShop() {
+        return new GoToShop(driver, context);
     }
 
-    public PlaceOrderCommand placeOrder() {
-        return new PlaceOrderCommand(driver, context);
+    public PlaceOrder placeOrder() {
+        return new PlaceOrder(driver, context);
     }
 
-    public CancelOrderCommand cancelOrder() {
-        return new CancelOrderCommand(driver, context);
+    public CancelOrder cancelOrder() {
+        return new CancelOrder(driver, context);
     }
 
-    public ViewOrderCommand viewOrder() {
-        return new ViewOrderCommand(driver, context);
+    public ViewOrder viewOrder() {
+        return new ViewOrder(driver, context);
     }
 
 }
