680287eee7d4c853e2027608ca3bd54e35162ad9
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-25 16:24:06 +0100

    Add base E2E test classes and implement positive order placement test

---
+++ system-test/e2e-test/src/test/java/com/optivem/eshop/systemtest/e2etests/v7/PlaceOrderPositiveTest.java
@@ -0,0 +1,24 @@
+package com.optivem.eshop.systemtest.e2etests.v7;
+
+import com.optivem.eshop.systemtest.core.shop.ChannelType;
+import com.optivem.eshop.systemtest.core.shop.client.dtos.enums.OrderStatus;
+import com.optivem.eshop.systemtest.e2etests.v6.base.BaseE2eTest;
+import com.optivem.testing.channels.Channel;
+import com.optivem.testing.channels.DataSource;
+import org.junit.jupiter.api.TestTemplate;
+
+import static com.optivem.eshop.systemtest.e2etests.commons.constants.Defaults.ORDER_NUMBER;
+import static com.optivem.eshop.systemtest.e2etests.commons.constants.Defaults.SKU;
+
+public class PlaceOrderPositiveTest extends BaseE2eTest {
+    @TestTemplate
+    @Channel({ChannelType.UI, ChannelType.API})
+    void shouldCalculateOriginalPrice() {
+        scenario
+                .given().product().withSku("ABC").withUnitPrice(20.00)
+                .when().placeOrder().withOrderNumber("ORDER-1001").withSku("ABC").withQuantity(5)
+                .then().shouldSucceed()
+                .and().order("ORDER-1001").shouldHaveOriginalPrice(100.00);
+    }
+}
+
---
+++ test-infrastructure/src/main/java/com/optivem/eshop/systemtest/base/v7/BaseScenarioDslTest.java
@@ -0,0 +1,27 @@
+package com.optivem.eshop.systemtest.base.v7;
+
+import com.optivem.eshop.systemtest.configuration.BaseConfigurableTest;
+import com.optivem.eshop.systemtest.core.SystemDsl;
+import com.optivem.eshop.systemtest.core.gherkin.ScenarioDsl;
+import com.optivem.lang.Closer;
+import com.optivem.testing.channels.ChannelExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(ChannelExtension.class)
+public class BaseScenarioDslTest extends BaseConfigurableTest {
+    protected ScenarioDsl scenario;
+
+    @BeforeEach
+    void setUp() {
+        var configuration = loadConfiguration();
+        var app = new SystemDsl(configuration);
+        scenario = new ScenarioDsl(app);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Closer.close(scenario);
+    }
+}
