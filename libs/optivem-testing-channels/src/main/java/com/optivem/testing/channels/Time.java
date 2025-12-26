b2c3711009fbcb0b045cf2313537d8d48f6b8a82
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-12-26 12:15:42 +0100

    Add Time annotation to mark tests requiring controlled time setup

---
+++ libs/optivem-testing-channels/src/main/java/com/optivem/testing/channels/Time.java
@@ -0,0 +1,23 @@
+package com.optivem.testing.channels;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to mark tests that require controlled time setup.
+ * Tests with this annotation should be executed separately as they depend on specific time configurations
+ * (e.g., for discount rates, time-based calculations, etc.).
+ * 
+ * Use this to filter/group tests that need time control vs those that don't.
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Time {
+    /**
+     * Whether this test requires controlled time setup.
+     * Default is true.
+     */
+    boolean required() default true;
+}
